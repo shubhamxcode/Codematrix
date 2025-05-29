9b8739b20ae70ff12e64d7aed89b46d4a0804383
shubhamxcode <shubh.varshneycode@gmail.com>
2025-05-29 16:17:37 +0530

    minor changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesReducer from "../Feature/addtofav/Addtofav"
-import theme from '../Feature/addtofav/Addtofav'
 export const store = configureStore({
   reducer:{
     favorites:favoritesReducer,
-    Theme:theme,
   },
 });
 
